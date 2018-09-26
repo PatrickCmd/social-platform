11c9a7bc6e1b3d4e7e60c7103bb256f47a830d1a
PatrickCmd <pwalukagga@gmail.com>
2018-09-26 18:34:18 +0300

    chore(configure postgres): configure to use postgresql database egine
    - add settings for the different configurations
    - enable postgresql as the database backend engine
    
    [Delivers #158656060]
    
    ch(configure postgres): decouple settings configuration
    
    - breaking settings configuration for the different environments
    - creating settings package and add local and production modules
    
    [Delivers #158656060]
    
    chore(configure postgres): add pipline .json file
    
    - add app.json file for pipelines during deployment
    
    [Delivers #158656060]
    
    chore(configure postgres): add deployment file
    
    - add procfile file for deployment on heroku
    
    [Delivers #158656060]
    
    chore(configure postgres): add application packages
    
    - add more application dependencies
    
    [Delivers #158656060]
    
    chore(configure postgres): step up different enviroments
    
    - add conditionals for the different environments
    
    [Delivers #158656060]
    
    chore(configure postgres): add badges
    
    - add travis and coveralls readme badges
    
    [Delivers #158656060]
    
    chore(configure postgres): add postgres configurations
    
    - delete settings module
    -create settings packaga and add different settings module for different environments
    
    [Delivers #158656060]

---
+++ authors/settings/production.py
@@ -0,0 +1,9 @@
+from .base import *
+
+import dj_database_url
+
+DEBUG = config('DEBUG', cast=bool)
+
+DATABASES['default'] = dj_database_url.config(
+    'default' = config('DATABASE_URL')
+)
--- authors/wsgi.py
+++ authors/wsgi.py
@@ -4,13 +4,16 @@ WSGI config for authors project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings")
+if os.environ.get('APP_SETTINGS') == 'production':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.production")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors.settings.local")
 
 application = get_wsgi_application()
