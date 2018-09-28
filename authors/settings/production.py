31b0220c68aea7af9f9cd24d004cfc12b129d684
PatrickCmd <pwalukagga@gmail.com>
2018-09-29 00:45:32 +0300

    fix(bug): fix database_url bug

--- authors/settings/production.py
+++ authors/settings/production.py
@@ -5,5 +5,5 @@ import dj_database_url
 DEBUG = config('DEBUG', cast=bool)
 
 DATABASES['default'] = dj_database_url.config(
-    'default' = config('DATABASE_URL')
+    default=config('DATABASE_URL')
 )
