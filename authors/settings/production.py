a0793ee9fd299241f6b651c333d73f95058fd265
PatrickCmd <pwalukagga@gmail.com>
2018-09-29 09:40:34 +0300

    fix(statics): add django-heroku settings

--- authors/settings/production.py
+++ authors/settings/production.py
@@ -1,12 +1,12 @@
-import django_heroku
-
 from .base import *
 
 import dj_database_url
 
 DEBUG = config('DEBUG', cast=bool)
-# django_heroku.settings(locals(), test_runner=False)
 
 DATABASES['default'] = dj_database_url.config(
     default=config('DATABASE_URL')
 )
+
+import django_heroku
+django_heroku.settings(locals(), test_runner=False)
